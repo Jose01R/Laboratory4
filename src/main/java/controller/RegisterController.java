a7efbc7e6ec86c5ba7c14de4658e3f17361dd8e6
Jose01_RJ <j.andresrj.01@gmail.com>
2025-04-04 18:32:06 -0600

    Register FXML - CONTROLLER
    Fxml esta listo, controller tiene la base, falta implementar funcionalidad a botones

---
+++ src/main/java/controller/RegisterController.java
@@ -0,0 +1,85 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+
+public class RegisterController
+{
+    @javafx.fxml.FXML
+    private TableColumn idTableColumn;
+    @javafx.fxml.FXML
+    private TableView studentTableview;
+    @javafx.fxml.FXML
+    private TableColumn nameTableColumn;
+    @javafx.fxml.FXML
+    private Button getFirstButton;
+    @javafx.fxml.FXML
+    private TableColumn ageTableColumn;
+    @javafx.fxml.FXML
+    private Button sizeButton;
+    @javafx.fxml.FXML
+    private BorderPane bp;
+    @javafx.fxml.FXML
+    private TableColumn addressTableColumn;
+    @javafx.fxml.FXML
+    private Button containsButton;
+    @javafx.fxml.FXML
+    private Button getLastButton;
+    @javafx.fxml.FXML
+    private Button removeFirstButton;
+    @javafx.fxml.FXML
+    private Button removeButton;
+    @javafx.fxml.FXML
+    private Button sortButton;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void addFirstOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void clearOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addSortedOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void getFirstOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void removeFirstOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void getLastOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void containsOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sortOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void sizeOnAction(ActionEvent actionEvent) {
+    }
+}
