6a1916d1a4a0c429bf632f37a9ff951cccc8e71c
jaredCR13 <jaredmor78@gmail.com>
2025-04-04 22:20:41 -0600

    nuevo

--- src/main/java/controller/RemoveStudentController.java
+++ src/main/java/controller/RemoveStudentController.java
@@ -14,8 +14,6 @@ public class RemoveStudentController {
     @javafx.fxml.FXML
     private BorderPane bp;
     @javafx.fxml.FXML
-    private TextField textFieldName;
-    @javafx.fxml.FXML
     private TextField textFieldId;
     private SinglyLinkedList studentList;
     private Alert alert; //para el manejo de alertas
@@ -32,7 +30,6 @@ public class RemoveStudentController {
     @javafx.fxml.FXML
     void clearOnAction(ActionEvent event) {
         textFieldId.clear();
-        textFieldName.clear();
     }
 
     @javafx.fxml.FXML
@@ -43,11 +40,10 @@ public class RemoveStudentController {
     @javafx.fxml.FXML
     void removeOnAction(ActionEvent event) {
         String id = textFieldId.getText().trim();
-        String name=textFieldName.getText().trim();
         try {
             for (int i = 1; i <= studentList.size(); i++) {
                 Student student = (Student) studentList.getNode(i).data;
-                if (util.Utility.compare(student.getId(),id)==0 && util.Utility.compare(student.getName(),name)==0) {
+                if (util.Utility.compare(student.getId(),id)==0) {
                     studentList.remove(student);
                     alert.setAlertType(Alert.AlertType.INFORMATION);
                     alert.setHeaderText("The student has been deleted.");
@@ -55,8 +51,8 @@ public class RemoveStudentController {
 
                     break;//Se sale del metodo si elimina un estudiante
                 }
-                if(textFieldName.getText().isEmpty()||textFieldId.getText().isEmpty()) {
-                    alert.setHeaderText("Both text fields have to be filled");
+                if(textFieldId.getText().isEmpty()) {
+                    alert.setHeaderText("text field have to be filled");
                     alert.show();
                 }else
                     alert.setAlertType(Alert.AlertType.ERROR);
@@ -69,7 +65,6 @@ public class RemoveStudentController {
             alert.show();
         }
         textFieldId.clear();
-        textFieldName.clear();
     }
 
 }
